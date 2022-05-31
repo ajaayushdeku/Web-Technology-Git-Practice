6341371b7bcfd6a01f3afffd7640d7e49a042473
Aayush Shrestha <be2020se698@gces.edu.np>
2022-05-31 18:03:56 +0545

    update

---
+++ Classwork/externaljs.js
@@ -0,0 +1,11 @@
+        // const btn1 =document.getElementById('btn1');
+        const btn1 =document.querySelector('#btn1');
+        btn1.addEventListener('click',function()
+        {
+            alert('Clicking');
+        })
+        // const btn2 =document.getElementById('btn2');
+        const btn2 =document.querySelector('#btn2');
+        btn2.addEventListener('mouseover',function(){
+            alert('Hovering');
+        })
